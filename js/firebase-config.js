37e7bd66a4eac529b06d751aa52bf67e92ddb586
Bembel Bowl <benjamin.ambros@gmail.com>
2026-09-20 23:37:57 +0200

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// Zentrale Firebase-Web-Konfiguration für Bembel Bowl.
+// WICHTIG: Bei einer Firebase Web App ist apiKey KEIN Server-Secret.
+// Schutz erfolgt über Firebase Auth, Firestore Security Rules und API-Key-Restriktionen.
+export const firebaseConfig = Object.freeze({
+  apiKey: "AIzaSyCvcSZmqVoLoLcUF4LiOHAfvyRmr3i7jOY",
+  authDomain: "bembel-bowl.firebaseapp.com",
+  projectId: "bembel-bowl",
+  storageBucket: "bembel-bowl.firebasestorage.app",
+  messagingSenderId: "836256439134",
+  appId: "1:836256439134:web:c39ef91c216da594020dcf"
+});
